f9297250c71c9a4d250dfc0250166f55274bcc6b
markhibberd <mark@hibberd.id.au>
2012-07-17 15:17:18 +1000

    Fail harder on error, add a devmode

--- src/scala/io/mth/foil/Jetty.scala
+++ src/scala/io/mth/foil/Jetty.scala
@@ -2,6 +2,7 @@ package io.mth.foil
 
 import org.eclipse.jetty.server.handler._
 import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.util.component._
 
 
 object Jetty {
@@ -12,6 +13,18 @@ object Jetty {
     server.setHandler(new ContextHandlerCollection)
     val binder = JettyBind.bind(config)
     binder(server)
+    server.addLifeCycleListener(new LifeCycle.Listener() {
+      def lifeCycleFailure(e: LifeCycle, c: Throwable) = {
+        System.err.println("Server stopped unexpectedly: "
+           c.getClass.getSimpleName + "[" + c.getMessage + "]" )
+        c.printStackTrace
+        sys.exit(1)
+      }
+      def lifeCycleStarted(e: LifeCycle) = {}
+      def lifeCycleStarting(e: LifeCycle) = {}
+      def lifeCycleStopped(e: LifeCycle) = {}
+      def lifeCycleStopping(e: LifeCycle) = {}
+    })
     server
   }
 }
